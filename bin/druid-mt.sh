b4dbc67e26e23eb1d2a9857d3e925c833cc3d88b
seoeun <seoeun25@gmail.com>
2018-11-06 20:42:28 +0900

    druid-metatron start stop

---
+++ bin/druid-mt.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+DRUID_HOME=/usr/local/druid-mt
+BIN=$DRUID_HOME/bin
+
+# export ZOO_LOG_DIR=$ZOOKEEPER_HOME/logs
+echo "BIN=$BIN"
+echo "druid $1"
+
+case "$1" in
+  start)
+	$BIN/coordinator.sh start
+	sleep 1
+	echo ""
+	$BIN/overlord.sh start
+	sleep 1
+	echo ""
+	$BIN/historical.sh start
+	sleep 1
+	echo ""
+	$BIN/middleManager.sh start
+	sleep 1
+	echo ""
+	$BIN/broker.sh start
+        exit 0
+        ;;
+  stop)
+        $BIN/broker.sh stop
+        $BIN/middleManager.sh stop
+        $BIN/historical.sh stop
+        $BIN/overlord.sh stop
+        $BIN/coordinator.sh stop
+        exit 0
+        ;;
+  *)
+        echo "Usage: $0 start|stop|" >&2
+        exit 3
+        ;;
+esac
+exit 6
